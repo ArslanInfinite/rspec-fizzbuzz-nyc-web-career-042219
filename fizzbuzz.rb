ed5d01e7613a81436b5b5333efc9a160b26b2b48
Arslan Ali <ali.arslan319@gmail.com>
2019-04-08 23:28:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-require 'pry'
-
-fizzbuzz
-binding.pry
+puts (1..100).map 
+  {|i|
+	f = i % 3 == 0 ? 'Fizz' : nil
+	b = i % 5 == 0 ? 'Buzz' : nil
+	f || b ? "#{ f }#{ b }" : i
+  }
