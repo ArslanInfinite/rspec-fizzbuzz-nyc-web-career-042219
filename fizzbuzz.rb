010f5a9ad3347b6c6ca19095d1a3ca7112e81a4a
Arslan Ali <ali.arslan319@gmail.com>
2019-04-08 23:28:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-puts (1..100).map 
+puts (1..100).map
   {|i|
 	f = i % 3 == 0 ? 'Fizz' : nil
 	b = i % 5 == 0 ? 'Buzz' : nil
